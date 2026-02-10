1184b45095990d78ec377647916c93e6f5bda1da
GrassIsComplex <eneandresioan@gmail.com>
2026-02-10 23:15:27 +0200

    Reflex game

--- joc/src/reflex.c
+++ joc/src/reflex.c
@@ -2,19 +2,19 @@
 #include "raylib.h"
 #include "screens.h"
 
-static Rectangle buttons[25];
+#define cnt 2
+
+static Rectangle buttons[cnt*cnt];
 
 static const double maxtime = 20.0f;
 static double gametime, changet, flyt;
 static bool gamestarted = false, gameover = false;
 
-static int score = 0, bad = -1, good = -1;
+static int score = 0, bad = -1, good = -1, max_score = 0;
 
 static void SetupRects(void) {
-	const int cnt = 5; // 4x4 grid
-
-	const int rect_l = 70;
-	const int gap = 8;
+	const int rect_l = 200;
+	const int gap = 6;
 
 	const int grid_w = cnt * rect_l + (cnt-1) * gap;
 	const int grid_h = cnt * rect_l + (cnt-1) * gap;
@@ -38,9 +38,8 @@ void ReflexEnterScreen() {
 	SetupRects();
 	good = -1;
 	bad = -1;
+	max_score = 0;
 };
-
-#include <stdio.h>
 
 void ReflexUpdate(){
 	if (!gameover) {
@@ -60,19 +59,20 @@ void ReflexUpdate(){
 			if (flyt <= 0.0f) good = bad = -1;
 			if (changet <= 0.0f) {
 				changet = 1.2f + rand()%50/100.0;
-				flyt = 1.0f;
+				flyt = 0.5f;
 				good = -1;
 				bad = -1;
-				if (rand()%2) good = rand()%25;
-				else bad = rand()%25;
-				puts(TextFormat("%.2f",changet));
+				if (rand()%2) {good = rand()%(cnt*cnt); max_score++;}
+				else bad = rand()%(cnt*cnt);
 			}
 			if (IsMouseButtonPressed(0)){
 				if (good != -1 && CheckCollisionPointRec(GetMousePosition(),buttons[good])){
 					flyt = 0.0f;
+					score++;
 				}
 				if (bad != -1 && CheckCollisionPointRec(GetMousePosition(),buttons[bad])){
 					flyt = 0.0f;
+					score--;
 				}
 			}
 		}
@@ -89,12 +89,14 @@ void ReflexDraw(){
 			DrawCenText("Fa clic pentru a incepe",GetScreenHeight()/2 - 20, 50);
 			DrawCenText("Apasa pe patratele verzi, dar nu pe cele albastre",GetScreenHeight()/2 + 25, 50);
 		} else {
-			for (int i = 0; i < 25; i++){
+			for (int i = 0; i < cnt*cnt; i++){
 				DrawRectangleRec(buttons[i], (good == i ? GREEN : ( bad == i ? BLUE : GRAY)));
 				DrawRectangleLinesEx(buttons[i], 2, BLACK);
 			}
+			DrawTextEx(font, TextFormat("Timp ramas: %01.02f", gametime), (Vector2){20, 20}, 40, 2, BLACK);
 		}
 	} else {
-		
+		DrawCenText("Felicitari!", GetScreenHeight()/2-40, 50);
+		DrawCenText(TextFormat("Ai obtinut %i/%i puncte", score >= 0 ? score : 0, max_score), GetScreenHeight()/2+12, 50);
 	}
 };
